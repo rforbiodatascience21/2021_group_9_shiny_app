958ed13655f1f41fa2a54421f4e053a868e55065
angelams98 <angelams98@gmail.com>
2021-04-12 10:18:45 +0000

    new functions added

--- R/app.R
+++ R/app.R
@@ -1,6 +1,8 @@
 # Load R packages
 library(shiny)
 library(shinythemes)
+library(tidyverse)
+library(lab08aSimpleRpackage)
 
 # Define UI
 ui <- fluidPage(theme = shinytheme("flatly"),
@@ -33,6 +35,7 @@ ui <- fluidPage(theme = shinytheme("flatly"),
                            sidebarPanel(
                              tags$h3("Input:"),
                              textAreaInput("dna", "Introduce your DNA chain"),
+                             fileInput("upload_dna", NULL, accept = "text/plain")
                            ),#sidebar panel end
                            #description
                            mainPanel(
@@ -42,11 +45,41 @@ ui <- fluidPage(theme = shinytheme("flatly"),
                              #OUTPUT
                              h3("Output:"),
                              textOutput("compl")
-                           ) #mainPanel ends
+                           )#main panel end
+                      ),
+                     tabPanel("DNA modifier",
+                              ### Input DNA or random gen
+                              h4("Generate random sequence or input your own"),
+                              
+                              div(style="display:inline-block",
+                                  numericInput("gen_dna_len", "Lenght of generated DNA sequence",  value=100, min = 3)
+                              ),
+                              div(style="display:inline-block",
+                                  actionButton("genDNA", "Generate Random DNA sequence")
+                              ),
+                              
+                              textAreaInput("dna_input", "DNA sequence", 
+                                            value="",
+                                            width = "600",
+                                            height = "120"),
+                              
+                              
+                              ### Create Codons
+                              h2("Codons"),
+                              numericInput("codons_start", "Position of initial codon", 
+                                           value=1, min=1),
+                              wellPanel(textOutput("codons")),
+                              
+                              ### Reverse compliment
+                              h2("Translate codons to AA"),
+                              wellPanel(textOutput("AA"))
+                              
+                              )#end of the tabpanel
+                           #mainPanel ends
                            
                            )# tabpanel end
                 ) # navbarPage end
-) # fluidPage end
+
 
 # SERVER
 server <- function(input, output) {
@@ -63,6 +96,39 @@ server <- function(input, output) {
     dna_split <- strsplit(input$dna, "")[[1]]
     dna_complement <- paste0(lookup[dna_split], collapse = "")
   })
+  
+  ### Generate random DNA
+  observeEvent(input$genDNA,{
+    updateTextAreaInput(inputId="dna_input",
+                        value=random_dna(input$gen_dna_len))
+  })
+  
+  
+  ### Split into codons
+  codons <- reactiveVal()
+  
+  observe({
+    if (str_length(input$dna_input) >= 3){
+      print(input$codons_start)
+      c_start = input$codons_start
+      if (!is.numeric(c_start) | c_start <= 0){c_start <- 1}
+      
+      
+      c = mk_codons(dna = input$dna_input, s = c_start)
+      codons(c)
+    }
+    else {
+      codons <- reactiveVal()
+    }
+    
+  })
+  
+  output$codons <- renderText(paste(codons(), sep=", "))
+  
+  ### To AA
+  output$AA <- renderText(dna_codons_to_aa(codons()))
+  
+  
 } #server end
 
 
