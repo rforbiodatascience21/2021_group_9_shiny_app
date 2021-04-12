03b9c5bac8fe8ec449258f1fcdd0939d1e60faa8
angelams98 <angelams98@gmail.com>
2021-04-12 10:31:13 +0000

    changes

--- R/app.R
+++ R/app.R
@@ -2,7 +2,7 @@
 library(shiny)
 library(shinythemes)
 library(tidyverse)
-library(lab08aSimpleRpackage)
+
 
 # Define UI
 ui <- fluidPage(theme = shinytheme("flatly"),
@@ -82,13 +82,13 @@ ui <- fluidPage(theme = shinytheme("flatly"),
                            sidebarPanel(
                              tags$h3("Input:"),
                              textInput("mk_codons_dna", "DNA Sequence:", "AUG..."),
-                             textInput("mk_codons_start", "Start Position:", 1),
+                             textInput("mk_codons_start", "Start Position:", 1)
                            ), # sidebarPanel
                            mainPanel(
                              h1("Results"),
                              
                              h4("Codons: "),
-                             verbatimTextOutput("mk_codons"),
+                             verbatimTextOutput("mk_codons")
                              
                            ) # mainPanel
                            
@@ -112,7 +112,6 @@ server <- function(input, output) {
     dna_complement <- paste0(lookup[dna_split], collapse = "")
   })
   
-<<<<<<< HEAD
   ### Generate random DNA
   observeEvent(input$genDNA,{
     updateTextAreaInput(inputId="dna_input",
@@ -142,10 +141,28 @@ server <- function(input, output) {
   output$codons <- renderText(paste(codons(), sep=", "))
   
   ### To AA
-  output$AA <- renderText(dna_codons_to_aa(codons()))
+  output$AA <- renderText({
+    std_code_table <- c("TTT" = "F", "TCT" = "S", "TAT" = "Y", "TGT" = "C",
+                        "TTC" = "F", "TCC" = "S", "TAC" = "Y", "TGC" = "C",
+                        "TTA" = "L", "TCA" = "S", "TAA" = "*", "TGA" = "*",
+                        "TTG" = "L", "TCG" = "S", "TAG" = "*", "TGG" = "W",
+                        "CTT" = "L", "CCT" = "P", "CAT" = "H", "CGT" = "R",
+                        "CTC" = "L", "CCC" = "P", "CAC" = "H", "CGC" = "R",
+                        "CTA" = "L", "CCA" = "P", "CAA" = "Q", "CGA" = "R",
+                        "CTG" = "L", "CCG" = "P", "CAG" = "Q", "CGG" = "R",
+                        "ATT" = "I", "ACT" = "T", "AAT" = "N", "AGT" = "S",
+                        "ATC" = "I", "ACC" = "T", "AAC" = "N", "AGC" = "S",
+                        "ATA" = "I", "ACA" = "T", "AAA" = "K", "AGA" = "R",
+                        "ATG" = "M", "ACG" = "T", "AAG" = "K", "AGG" = "R",
+                        "GTT" = "V", "GCT" = "A", "GAT" = "D", "GGT" = "G",
+                        "GTC" = "V", "GCC" = "A", "GAC" = "D", "GGC" = "G",
+                        "GTA" = "V", "GCA" = "A", "GAA" = "E", "GGA" = "G",
+                        "GTG" = "V", "GCG" = "A", "GAG" = "E", "GGG" = "G")
+    aa <- paste0(std_code_table[input$codons], collapse = "")
+  })
   
-  
-=======
+
+
   output$mk_codons <- renderText({
     s = as.integer(input$mk_codons_start)
     l = nchar(input$mk_codons_dna)
@@ -155,7 +172,7 @@ server <- function(input, output) {
     return(codons)
   })
   
->>>>>>> a0b3b29a8e1b473183cd3948342e1f35d5fb66e1
+
 } #server end
 
 
