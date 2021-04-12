b4f4ef416af2d814d0ddc988bad962f898b3e6fd
idafrichhansen <s174601@student.dtu.dk>
2021-04-12 08:55:25 +0000

    Add RandomRNA app

---
+++ R/app.R
@@ -0,0 +1,49 @@
+# Load R packages
+library(shiny)
+library(shinythemes)
+
+# Define UI
+ui <- fluidPage(theme = shinytheme("flatly"),
+                navbarPage(
+                  "Group 9 ShinyApp",
+                  tabPanel("Random RNA",
+                           
+                           # INPUT
+                           sidebarPanel(
+                             tags$h3("Input:"),
+                             sliderInput(
+                               inputId = "rna_length",
+                               label = "Length of RNA strand:",
+                               min = 1, value = 30, max = 200
+                               ) # sliderInput end
+                             ), # sidebarPanel end
+                           
+                           # DESCRIPTION
+                           mainPanel(
+                             h1("Description"),
+                             "Select a number with the slider, and a random RNA sequence of that lenght is generated.",
+                             
+                             # OUTPUT
+                             h3("Output"),
+                             verbatimTextOutput(outputId = "txtout")
+                           ) # mainpanel end
+                  ) # tabpanel end
+                ) # navbarPage end
+) # fluidPage end
+
+# SERVER
+server <- function(input, output) {
+  
+  output$txtout <-  renderText({
+    
+    nucleotides <- sample(c("A", "U", "G", "C"), size = input$rna_length, replace = TRUE)
+    rna = paste(nucleotides, collapse = "")
+    return(rna)
+    })
+} #server end
+
+
+# Create Shiny object
+shinyApp(ui = ui, server = server)
+
+
